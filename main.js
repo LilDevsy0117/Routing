322af6ee4ead7314214a33995ea3598d3ef7d00c
LilDevsy0117 <skkulsy0117@naver.com>
2022-10-13 14:31:32 +0000

    map

--- main.js
+++ main.js
@@ -6,3 +6,48 @@ toggleBtn.addEventListener('click', () => {
 });
 
 $('#lang').selectpicker();
+
+//---------------------openlayer----------------
+
+$(document).ready(function() {
+	init();
+});
+
+function init() {
+
+	// map 생성
+	var map1 = new ol.Map({
+		layers: [
+			new ol.layer.Tile({
+				source: new ol.source.OSM()     // OpenStreetMap 레이어
+			})
+		],
+		target: 'map1',                          // Map 생성할 div id
+		view: new ol.View({
+			center: [54200000, 4300000],        // 초기 지도 위치 좌표
+			zoom: 6                             // 초기 지도 위치 줌레벨
+		})
+	});
+
+	var map2 = new ol.Map({
+		layers: [
+			new ol.layer.Tile({
+				source: new ol.source.OSM()     // OpenStreetMap 레이어
+			})
+		],
+		target: 'map2',                          // Map 생성할 div id
+		view: new ol.View({
+			center: [54200000, 4300000],        // 초기 지도 위치 좌표
+			zoom: 6                             // 초기 지도 위치 줌레벨
+		})
+	});
+	// 레이어 추가
+	/*
+	var osmLayer = new ol.layer.Tile({
+		source: new ol.source.OSM()
+	});
+	map.addLayer(osmLayer);
+	*/
+
+}
+
